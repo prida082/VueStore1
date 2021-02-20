2f968cc763f8ea4d09682e3cb8443cbf1d24faf0
prida082 <beatbrave882@gmail.com>
2021-02-21 04:41:58 +0700

    new commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="text-danger">
+      ...Please select order...
+    </div>
+    <div class="row">
+      <product
+        v-for="(item, index) in Menuitem"
+        :key="index"
+        :NameProduct="item.NameProduct"
+        :img="item.img"
+        :Price="item.Price"
+        @Order="menuorder"
+      />
+    </div>
   </div>
 </template>
+<script>
+import Product from "@/components/Product.vue";
+export default {
+  components: { Product },
+  props: {
+    Menuitem: Array,
+  },
+  data() {
+    return {
+      select: [],
+    };
+  },
+  methods: {
+    menuorder(value) {
+      this.select.push(value);
+      this.$emit("Order", this.select);
+    },
+  },
+};
+</script>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <user :Total="Total" :Data="Data" />
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Orders</th>
+          <th scope="col">Price</th>
+          <th scope="col">Number</th>
+          <th scope="col">Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(mi, index) in Data" :key="index">
+          <td>{{ mi.NameProduct }}</td>
+          <td>{{ mi.Price }}</td>
+          <td>{{ mi.Amount }}</td>
+          <td>{{ mi.Total }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import User from "@/components/User.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    User,
+  },
+  props: {
+    Total: Number,
+    Data: Array,
+  },
+  data() {
+    return {};
+  },
 };
 </script>
